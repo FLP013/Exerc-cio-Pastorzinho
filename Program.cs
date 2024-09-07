03d0d7f4f4f6bd6bb97938d3716fb7b9a369c834
FLP013 <felliperibeiro9@gmail.com>
2024-09-07 00:57:13 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿//Dó=1320 Ré=1485 Mi=1650 Fá=1759 Sol=1980 Lá=2200 Si=2475
+Console.Clear();
+ 
+Console.Write("Dó, "); Console.Beep(1320, 200);
+Console.Write("Ré, "); Console.Beep(1485, 200);
+Console.Write("Mi, "); Console.Beep(1650, 200);
+Console.Write("Fá, "); Console.Beep(1759, 200);
+Thread.Sleep(500);
+Console.Write("Fá, "); Console.Beep(1759, 200);
+Console.Write("Fá, "); Console.Beep(1759, 200); Thread.Sleep(1000);
+ 
+Console.WriteLine();
+ 
+Console.Write("Dó, "); Console.Beep(1320, 200);
+Console.Write("Ré, "); Console.Beep(1485, 200);
+Console.Write("Dó, "); Console.Beep(1320, 200);
+Console.Write("Ré, "); Console.Beep(1485, 200);
+Thread.Sleep(500);
+Console.Write("Ré, "); Console.Beep(1485, 200);
+Console.Write("Ré, "); Console.Beep(1485, 200); Thread.Sleep(1000);
+ 
+Console.WriteLine();
+ 
+Console.Write("Dó, "); Console.Beep(1320, 200);
+Console.Write("Sol, "); Console.Beep(1980, 200);
+Console.Write("Fá, "); Console.Beep(1759, 200);
+Console.Write("Mi, "); Console.Beep(1650, 200);
+Thread.Sleep(500);
+Console.Write("Mi, "); Console.Beep(1650, 200);
+Console.Write("Mi, "); Console.Beep(1650, 200); Thread.Sleep(1000);
+ 
+Console.WriteLine();
+ 
+Console.Write("Dó, "); Console.Beep(1320, 200);
+Console.Write("Ré, "); Console.Beep(1485, 200);
+Console.Write("Mi, "); Console.Beep(1650, 200);
+Console.Write("Fá, "); Console.Beep(1759, 200);
+Thread.Sleep(500);
+Console.Write("Fá, "); Console.Beep(1759, 200);
+Console.Write("Fá, "); Console.Beep(1759, 200); Thread.Sleep(1000);
